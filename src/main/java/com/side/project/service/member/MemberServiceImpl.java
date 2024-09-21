a3657c58f175b8b8ed538fe93082d8d11b39fdde
oniic <onisic95@gmail.com>
2024-09-22 01:29:39 +0900

    어노테이션 수정

--- src/main/java/com/side/project/service/member/MemberServiceImpl.java
+++ src/main/java/com/side/project/service/member/MemberServiceImpl.java
@@ -6,11 +6,12 @@ import com.side.project.repository.member.MemberRepository;
 import lombok.AllArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Repository;
+import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-@Repository
+@Service
 @AllArgsConstructor
-@Transactional
+@Transactional(readOnly = true)
 @Log4j2
 public class MemberServiceImpl implements MemberService {
 
@@ -19,6 +20,7 @@ public class MemberServiceImpl implements MemberService {
     /*
     * 회원 저장
     */
+    @Transactional
     @Override
     public Long save(JoinMemberDTO joinMemberDTO) throws Exception {
 
